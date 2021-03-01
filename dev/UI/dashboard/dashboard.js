6cf100afe7af1711b5bd6c62ed50793e894fe0e3
Nordine MARIE <nordine.marie@telecom-sudparis.eu>
2021-03-01 15:24:45 +0100

    UPDATE UI

--- dev/UI/dashboard/dashboard.js
+++ dev/UI/dashboard/dashboard.js
@@ -2,5 +2,12 @@ myApp.controller('DashboardController', ["$scope", "$state", "$window", "$http",
 
   function($scope, $state, $window, $http) {
     console.log("DockerINT") ;
+    const progress = document.querySelector('.progress-done');
+    console.log(progress) ;
+    progress.style.width = progress.getAttribute('data-done') + '%';
+    progress.style.opacity = 1;
+
+
+
   }
 ]);
